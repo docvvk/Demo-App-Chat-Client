0537c72b437023d1705f99d3181484289f4983fd
Vivek Malhan <vivek.malhan@yahoo.com>
2018-07-24 13:52:12 -0400

    working front and back end

--- assets/javascript/server.js
+++ assets/javascript/server.js
@@ -1,17 +1,25 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
 var messages = [
-    {name: 'Vivek', message: 'Hi'},
-    {name: 'Pooja', message: 'Hey'}
-];
+    {name: 'Tim', message: 'Hi'},
+    {name: 'Jane', message: 'Hello'}
+]
 
-app.get('/messages', (req,res)=> {
-    res.send(messages);
+app.get('/messages', (req, res) =>{
+    res.send(messages)
+})
+
+app.post('/messages', (req, res) =>{
+    messages.push(req.body)
+    res.sendStatus(200)
 })
 
 var server = app.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
-});
+})
